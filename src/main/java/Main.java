93e8a8baedd6e7405c88b6347808b28895d04a04
Dary <Dafna_pelmeshka@mail.ru>
2023-05-02 20:37:24 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = "13";
+        post.birthday.month = "06";
+        post.birthday.year = "1999";
+        post.name = "Дарья";
+        post.passport = "1234 № 567890";
+        post.patronymic = "Леонидовна";
+        post.phone = "+7 (999) 1234567";
+        post.surname = "Петрова";
+        post.subscription = true;
+    }
+}
